15a00e92413cf56c37f7d49906922e362fe389a7
Raymond Menzel <rlmenzel@login1.ascent.olcf.ornl.gov>
2019-06-18 10:32:33 -0400

    fix bugs in submit script

--- submit.sh
+++ submit.sh
@@ -2,22 +2,27 @@
 
 
 function usage() {
-    printf "usage: $0 [-h] [-n <name>] [-u] executable walltime ranks gpus\n"
+    printf "usage: $0 [-h] [-n <name>] [-u] [-w <directory>] executable walltime ranks gpus\n"
 }
 
 
 function help_mesg() {
     usage
     cat <<EOF
+
 Positional arguments:
-executable          Program to run.
-walltime            Wall clock time for run in minutes.
-ranks               Number of MPI ranks to use.
-gpus                Number of GPUs to use.
+executable                         Program to run.
+walltime                           Wall clock time for run in minutes.
+ranks                              Number of MPI ranks to use.
+gpus                               Number of GPUs to use.
+
 Optional arguments:
--h,--help           Prints this help message.
--n,--name <name>    Name of the job.
--u,--unique         Append timestamp on runscript
+-h,--help                          Prints this help message.
+-n,--name <name>                   Name of the job.  Defaults to "job".
+-u,--unique                        Append timestamp on runscript.
+-w,--work-directory <directory>    Directory to run the executable in (will be created
+                                   if it doesn't exist).  Defaults to 
+                                   /gpfs/wolf/gen127/scratch/$USER/princeton_gpu_hackathon.
 EOF
 }
 
@@ -26,6 +31,7 @@ EOF
 pos_args=("" "" "" "")
 c=0
 jobname="job"
+workdir="/gpfs/wolf/gen127/scratch/$USER/princeton_gpu_hackathon"
 while [[ $# -gt 0 ]]; do
     arg="$1"
     case $arg in
@@ -46,6 +52,15 @@ while [[ $# -gt 0 ]]; do
             unique="true"
             shift
             ;;
+        -w|--work-directory)
+            workdir="$2"
+            if [ -z "$workdir" ]; then
+                usage
+                exit 1
+            fi
+            shift
+            shift
+            ;;
         *)
             pos_args[$c]=$1
             c=$((c+1))
@@ -62,14 +77,15 @@ walltime=${pos_args[1]}
 ranks=${pos_args[2]}
 gpus=${pos_args[3]}
 
-
 #Create submission script.
-script="run_$executable"
+execname=`basename $executable`
+script="run_${execname}"
 if [ ! -z "$unique" ]; then
     now=`date +%s`
     script="${script}.${now}"
 fi
 cat > $script << EOF
+#!/bin/bash -xe
 #BSUB -P GEN127
 #BSUB -W 00:${walltime}
 #BSUB -nnodes 1
@@ -77,8 +93,10 @@ cat > $script << EOF
 #BSUB -o ${jobname}.%J
 #BSUB -e ${jobname}.%J
 
-cd /gpfs/wolf/gen127/scratch/$USER
-jsrun --nrs 1 --tasks_per_rs $ranks --cpu_per_rs $ranks --gpu_per_rs $gpus $executable
+mkdir -p $workdir
+cp -f $executable $workdir
+cd $workdir
+jsrun --nrs 1 --tasks_per_rs $ranks --cpu_per_rs $ranks --gpu_per_rs $gpus ./$execname
 EOF
 
 #Submit the script
